8d608cc3f527b43ff3360af7eb04425a096ec924
kobosky <117521539+kobosky@users.noreply.github.com>
2023-05-03 23:13:05 +0100

    shared

--- e-learning-infrastructure-ecs/provider.tf
+++ e-learning-infrastructure-ecs/provider.tf
@@ -1,7 +1,7 @@
 # configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
   region                  = var.region
-  shared_credentials_file = "C:/Users/Kobosky/.aws/credentials"
+  
 
   default_tags {
     tags = {
